bda257720a1187bfd24238837e4bcb3650045257
LunaCI <lunaci@luadist.org>
2018-06-08 01:50:24 +0000

    Update to version 0.1.0.beta2-1

---
+++ luapak/init.lua
@@ -0,0 +1,10 @@
+---------
+-- The main module.
+----
+
+return {
+  --- Luapak version number.
+  _VERSION = '0.1.0.beta2',
+  --- Luapak homepage.
+  _HOMEPAGE = 'https://github.com/jirutka/luapak',
+}
